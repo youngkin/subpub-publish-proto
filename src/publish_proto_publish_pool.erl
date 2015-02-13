7721570429e685806dd81ec85d312fa7acecaf0f
Rich Youngkin <rich.youngkin@gmail.com>
2015-02-13 16:46:10 -0700

    adding exometer support.

--- src/publish_proto_publish_pool.erl
+++ src/publish_proto_publish_pool.erl
@@ -33,7 +33,7 @@
 -define(SERVER, ?MODULE).
 -define(PUBLISH_STATS_DELAY, 30000). %% 30 seconds
 
--record(state, {publisher_pids = [], next_publish_worker = 1, pub_duration_list = []}).
+-record(state, {publisher_pids = [], next_publish_worker = 1, pub_duration_list = [], last_sample_time}).
 
 %% ====================================================================
 %% API functions
@@ -54,15 +54,18 @@ start_link() ->
 
 init([]) ->
   lager:info("Started publish_proto_publish gen_server"),
-  erlang:send_after(?PUBLISH_STATS_DELAY, self(), publish_stats),
+  configure_stats(),
+  
+  erlang:send_after(?PUBLISH_STATS_DELAY, self(), log_stats),
   PublisherPids = start_publishers(publish_proto_config:get(publishing_pool_size)),
-  {ok, #state{publisher_pids = PublisherPids}}.
+  LastSampleTime = get_now_in_gregorian_seconds(),
+  {ok, #state{publisher_pids = PublisherPids, last_sample_time = LastSampleTime}}.
 
 %%
 %% Publishes to a pool of publisher workers chosen in a round-robin fashion
 %%
 handle_call(publish_message, _From, #state{publisher_pids = PublisherPids, next_publish_worker = NextPublishWorker,
-  pub_duration_list = PublishDurationList} = _State) ->
+  pub_duration_list = PublishDurationList, last_sample_time = LastSampleTime} = _State) ->
   PublisherPid = lists:nth(NextPublishWorker, PublisherPids),
   %% TODO
   %% TODO need a receive loop here to make this call synchronous? 
@@ -77,7 +80,7 @@ handle_call(publish_message, _From, #state{publisher_pids = PublisherPids, next_
         NextPublishWorker + 1
     end,
   {reply, ok, #state{publisher_pids = PublisherPids, next_publish_worker = NewNextPublishWorker,
-    pub_duration_list = PublishDurationList}};
+    pub_duration_list = PublishDurationList, last_sample_time = LastSampleTime}};
 handle_call(Request, _From, State) ->
   lager:warning("Unknown call: ~p", [Request]),
   {reply, ok, State}.
@@ -89,10 +92,22 @@ handle_cast(Request, State) ->
 %%
 %% handle_info
 %%
-handle_info(publish_stats, State) ->
+%%
+%% TODO this function can probably go away when the transition to exometer is complete
+%%
+handle_info(log_stats, State) ->
+  %% TODO had to remove the #state{...} = State pattern matching in the argument list because it
+  %% TODO wasn't working. Come back and figure out why.
   PublisherPids = State#state.publisher_pids,
   NextPublishWorker = State#state.next_publish_worker,
   PublishDurationList = State#state.pub_duration_list,
+  LastSampleTime = State#state.last_sample_time,
+  lager:info("LastSampleTime = ~p", [LastSampleTime]),
+  
+  %% TODO DEBUG only, remove
+%%   exometer:update([erlang,random,integer], 500),
+%%   exometer:update([publish_proto, publish, latency], 13),
+
   case length(PublishDurationList) of
     0 ->
       AvgPubTime = 0,
@@ -108,17 +123,27 @@ handle_info(publish_stats, State) ->
   lager:info("Publish interval stats (in millis): Min = ~p, Max = ~p, Median = ~p, Avg = ~p",
     [MinPubTime, MaxPubTime, MedianPubTime, AvgPubTime]),
   NewState = #state{publisher_pids = PublisherPids, next_publish_worker = NextPublishWorker,
-    pub_duration_list = []},
-  erlang:send_after(?PUBLISH_STATS_DELAY, self(), publish_stats),
+    pub_duration_list = [], last_sample_time = LastSampleTime},
+  erlang:send_after(?PUBLISH_STATS_DELAY, self(), log_stats),
   {noreply, NewState };
 
 handle_info({record_stats, PubDurationMillis}, State) ->
+  %% TODO had to remove the #state{...} = State pattern matching in the argument list because it
+  %% TODO wasn't working. Come back and figure out why.
   PublisherPids = State#state.publisher_pids,
   NextPublishWorker = State#state.next_publish_worker,
   PublishDurationList = State#state.pub_duration_list,
+  LastSampleTime = State#state.last_sample_time,
   NewPubDurationList = [PubDurationMillis | PublishDurationList],
+  
+  %%%
+  %%% Publishes stats to exometer. Long term this will replace the rest of this
+  %%% function and handle_info/log_stats, and most of the state currently managed
+  %%% in support of capturing stats (e.g., pub_duration_list)
+  %%%
+  NewLastSampleTime = publish_stats(PubDurationMillis, LastSampleTime),
   {noreply, #state{publisher_pids = PublisherPids, next_publish_worker = NextPublishWorker,
-    pub_duration_list = NewPubDurationList} };
+    pub_duration_list = NewPubDurationList, last_sample_time = NewLastSampleTime} };
 
 handle_info({'EXIT', _Pid, killed}, State) ->
   lager:warning("Publisher worker killed"),
@@ -186,4 +211,41 @@ calc_publish_time_median(PubDurationsList) ->
              lists:nth(MedianPosition, SortedList)
          end
   end.
+
+get_now_in_gregorian_seconds() ->
+  calendar:datetime_to_gregorian_seconds(calendar:now_to_datetime(now())).
+
+publish_stats(PubDurationMillis, LastSampleTime) ->
+  SecsSinceLastPublished = get_now_in_gregorian_seconds() - LastSampleTime,
+  case SecsSinceLastPublished of
+    N when N >= 1 -> %% publish stats 1/sec
+      %% TODO: debug remove
+      lager:info("Publish latency: ~p (millis)", [PubDurationMillis]),
+
+      exometer:update([publish_proto,publish,latency], PubDurationMillis),
+      %% TODO DEBUG only, remove
+      exometer:update([erlang,random,integer], random:uniform(100)),
+
+      get_now_in_gregorian_seconds();
+    _ -> 
+      LastSampleTime
+  end.
+
+configure_stats() ->
+  ok = exometer:new([publish_proto, erlang, memory],
+    {function, erlang, memory, ['$dp'], value,
+      [total, processes, system, atom, binary, ets]}),
+
+  % Memory actively used by the VM, allocated (should ~match OS allocation),
+  % unused (i.e. allocated - used), and usage (used / allocated).
+  ok = exometer:new([publish_proto, recon, alloc],
+    {function, recon_alloc, memory, ['$dp'], value,
+      [used, allocated, unused, usage]}),
+
+  % TODO Debug metric, does it work?
+  exometer:new([erlang,random,integer], histogram, [{time_span, 600000}]),
+
+  % time_span is one of the histogram options. It's only shown to highlight that there are
+  % options. See the exometer_core project in github, exometer_histogram.erl, for details.
+  ok = exometer:new([publish_proto,publish,latency], histogram, [{time_span, 600000}, {slot_period, 10000}]).
 
